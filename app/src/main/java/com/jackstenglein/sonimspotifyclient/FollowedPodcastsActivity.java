2ebb9c11073f155a3b3add8004306fd1bc08c4f7
Jack Stenglein <jackstenglein@utexas.edu>
2021-02-14 12:02:45 -0600

    Add empty activities for main screens

---
+++ app/src/main/java/com/jackstenglein/sonimspotifyclient/FollowedPodcastsActivity.java
@@ -0,0 +1,14 @@
+package com.jackstenglein.sonimspotifyclient;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FollowedPodcastsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.primary_secondary_list);
+    }
+}
---
+++ app/src/main/java/com/jackstenglein/sonimspotifyclient/NowPlayingActivity.java
@@ -0,0 +1,14 @@
+package com.jackstenglein.sonimspotifyclient;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NowPlayingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.now_playing_activity);
+    }
+}
