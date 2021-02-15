0a9aedb56ba16bc690e7152acb23909c2d10c9c6
Jack Stenglein <jackstenglein@utexas.edu>
2021-02-14 20:05:49 -0600

    Add now playing info to home activity

--- app/src/main/java/com/jackstenglein/sonimspotifyclient/HomeActivity.java
+++ app/src/main/java/com/jackstenglein/sonimspotifyclient/HomeActivity.java
@@ -3,14 +3,30 @@ package com.jackstenglein.sonimspotifyclient;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import java.util.HashMap;
+import java.util.Locale;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.CallResult;
+import com.spotify.protocol.types.Image;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.auth.AuthorizationClient;
+import com.spotify.sdk.android.auth.AuthorizationRequest;
+import com.spotify.sdk.android.auth.AuthorizationResponse;
+import com.yashovardhan99.timeit.Stopwatch;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements Stopwatch.OnTickListener {
 
     private enum SelectableItem {
 
@@ -60,7 +76,31 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private static final String TAG = "HomeActivity";
+    private static final int REQUEST_CODE = 1326;
+    private static final String REDIRECT_URI = "sonimspotifyclient://callback";
+    private static final String CLIENT_ID = "4a00bb1466e04ec5885388b05fb44a79";
+    private static final String[] SPOTIFY_SCOPES = new String[] {"app-remote-control",
+            "playlist-read-private", "user-library-read", "user-library-modify",
+            "user-read-playback-position" };
+    private static final String TIME_FORMAT = "%d:%02d";
+    private static final int SPOTIFY_POLLING_DELAY_MS = 500;
 
+    // Spotify connection variables
+    private String spotifyAccessCode;
+    private SpotifyAppRemote spotifyAppRemote;
+
+    // Now playing UI elements and polling timer
+    private Stopwatch stopwatch;
+    private Track currentTrack;
+    private View nowPlayingInfoContainer;
+    private ImageView albumImage;
+    private TextView songTitle;
+    private TextView artistName;
+    private TextView timeElapsed;
+    private TextView totalTime;
+    private ProgressBar progressBar;
+
+    // UI elements to display selected item
     private SelectableItem currentSelection;
     private Drawable selectedBackground;
     private final HashMap<SelectableItem, View> selectableViews = new HashMap<>();
@@ -69,14 +109,150 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
-        selectedBackground = ContextCompat.getDrawable(this, R.drawable.selected_item);
 
+        // Get now playing UI elements
+        nowPlayingInfoContainer = findViewById(R.id.nowPlayingInfoContainer);
+        albumImage = findViewById(R.id.albumImage);
+        songTitle = findViewById(R.id.songTitle);
+        artistName = findViewById(R.id.artistName);
+        timeElapsed = findViewById(R.id.timeElapsed);
+        totalTime = findViewById(R.id.totalTime);
+        progressBar = findViewById(R.id.progressBar);
+
+        // Get UI elements for displaying selected item
+        selectedBackground = ContextCompat.getDrawable(this, R.drawable.selected_item);
         currentSelection = SelectableItem.Search;
         for (SelectableItem item : SelectableItem.values()) {
             View view = findViewById(item.getViewID());
             assert(view != null);
             selectableViews.put(item, view);
         }
+
+        // Connect to Spotify
+        AuthorizationRequest.Builder builder = new AuthorizationRequest.Builder(CLIENT_ID,
+                AuthorizationResponse.Type.CODE, REDIRECT_URI);
+        builder.setScopes(SPOTIFY_SCOPES);
+        AuthorizationRequest request = builder.build();
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthorizationResponse response = AuthorizationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case CODE:
+                    spotifyAccessCode = response.getCode();
+                    Log.d(TAG, "Spotify Access Code: " + spotifyAccessCode);
+                    connectToSpotifyAppRemote();
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    Log.e(TAG, "Error response from Spotify AuthorizationClient",
+                            new RuntimeException(response.getError()));
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+            }
+        }
+    }
+
+    private void connectToSpotifyAppRemote() {
+        ConnectionParams connectionParams = new ConnectionParams.Builder(CLIENT_ID)
+                .setRedirectUri(REDIRECT_URI).showAuthView(false).build();
+        SpotifyAppRemote.connect(this, connectionParams, new Connector.ConnectionListener(){
+
+            @Override
+            public void onConnected(SpotifyAppRemote remote) {
+                spotifyAppRemote = remote;
+                Log.d(TAG, "Connected to spotify app remote");
+                subscribeToPlayerState();
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                Log.e(TAG, throwable.getMessage(), throwable);
+            }
+        });
+    }
+
+    private void subscribeToPlayerState() {
+        stopwatch = new Stopwatch();
+        stopwatch.setClockDelay(SPOTIFY_POLLING_DELAY_MS);
+        stopwatch.setOnTickListener(this);
+        stopwatch.start();
+    }
+
+    @Override
+    public void onTick(Stopwatch stopwatch) {
+        spotifyAppRemote.getPlayerApi().getPlayerState().setResultCallback(
+            new CallResult.ResultCallback<PlayerState>() {
+                @Override
+                public void onResult(PlayerState playerState) {
+                    updateNowPlayingInformation(playerState);
+                }
+        });
+    }
+
+    private void updateNowPlayingInformation(PlayerState playerState) {
+        Track track = playerState.track;
+        if (track == null) {
+            Log.d(TAG, "No track playing.");
+            currentTrack = null;
+            nowPlayingInfoContainer.setVisibility(View.INVISIBLE);
+            return;
+        }
+
+        updateElapsedTime(playerState);
+
+        if (track.equals(currentTrack)) return;
+
+        Log.d(TAG, "Playing track: " + track.name);
+        currentTrack = track;
+
+        songTitle.setText(track.name);
+        artistName.setText(track.artist.name);
+        progressBar.setMax((int)track.duration);
+        totalTime.setText(String.format(Locale.getDefault(),TIME_FORMAT, getMinutes(track.duration),
+                getSeconds(track.duration)));
+
+        spotifyAppRemote.getImagesApi().getImage(track.imageUri, Image.Dimension.SMALL)
+                .setResultCallback(new CallResult.ResultCallback<Bitmap>() {
+                    @Override
+                    public void onResult(Bitmap bitmap) {
+                        albumImage.setImageBitmap(bitmap);
+                    }
+                });
+
+        nowPlayingInfoContainer.setVisibility(View.VISIBLE);
+    }
+
+    private void updateElapsedTime(PlayerState playerState) {
+        timeElapsed.setText(String.format(Locale.getDefault(),TIME_FORMAT,
+            getMinutes(playerState.playbackPosition), getSeconds(playerState.playbackPosition)));
+        progressBar.setProgress((int)playerState.playbackPosition, true);
+    }
+
+    private long getMinutes(long duration) {
+        return (duration / 1000) / 60;
+    }
+
+    private long getSeconds(long duration) {
+        return (duration / 1000) % 60;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(spotifyAppRemote);
+        stopwatch.stop();
     }
 
     @Override
