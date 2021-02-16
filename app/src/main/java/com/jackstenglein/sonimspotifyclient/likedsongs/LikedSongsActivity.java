6a42cfc8598ad832e6615c659bb268037fc98ece
Jack Stenglein <jackstenglein@utexas.edu>
2021-02-16 16:10:02 -0600

    Refactor LikedSongsActivity into AbstractListActivity

--- app/src/main/java/com/jackstenglein/sonimspotifyclient/likedsongs/LikedSongsActivity.java
+++ app/src/main/java/com/jackstenglein/sonimspotifyclient/likedsongs/LikedSongsActivity.java
@@ -1,76 +1,45 @@
 package com.jackstenglein.sonimspotifyclient.likedsongs;
 
-import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.jackstenglein.sonimspotifyclient.HomeActivity;
-import com.jackstenglein.sonimspotifyclient.R;
+import com.jackstenglein.sonimspotifyclient.list.AbstractListActivity;
 import com.jackstenglein.sonimspotifyclient.list.PagerAdapter;
-import com.spotify.android.appremote.api.ConnectionParams;
-import com.spotify.android.appremote.api.Connector;
-import com.spotify.android.appremote.api.SpotifyAppRemote;
-
 import java.util.HashMap;
 import java.util.Locale;
-
-import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyCallback;
 import kaaes.spotify.webapi.android.SpotifyError;
-import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Pager;
 import kaaes.spotify.webapi.android.models.SavedTrack;
 import retrofit.client.Response;
 
-public class LikedSongsActivity extends AppCompatActivity implements PagerAdapter.DataSource<SavedTrack> {
+public class LikedSongsActivity extends AbstractListActivity<SavedTrack> implements PagerAdapter.DataSource<SavedTrack> {
 
     private static final String TAG = "LikedSongsActivity";
     private static final String ARTIST_NAME_AND_DURATION_FORMAT = "%s • %d:%02d";
 
-    private PagerAdapter<SavedTrack> adapter;
-    private SpotifyService spotifyWebApi;
-    private SpotifyAppRemote spotifyAppRemote;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.primary_secondary_list);
-
-        RecyclerView songsList = findViewById(R.id.primarySecondaryList);
-        songsList.setHasFixedSize(true);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        adapter = PagerAdapter.create(this, layoutManager);
-        songsList.setAdapter(adapter);
-        songsList.setLayoutManager(layoutManager);
-
-        connectToSpotifyAppRemote();
+    protected String getTag() {
+        return TAG;
     }
 
-    private void connectToSpotifyAppRemote() {
-        ConnectionParams connectionParams = new ConnectionParams.Builder(HomeActivity.CLIENT_ID)
-                .setRedirectUri(HomeActivity.REDIRECT_URI).showAuthView(false).build();
-        SpotifyAppRemote.connect(this, connectionParams, new Connector.ConnectionListener(){
+    @Override
+    protected void handleSelection(SavedTrack savedTrack) {
+        if (savedTrack != null && spotifyAppRemote != null) {
+            Log.d(TAG, "playing " + savedTrack.track.name + " with URI: " +
+                    savedTrack.track.uri);
+            spotifyAppRemote.getPlayerApi().play(savedTrack.track.uri);
+        }
+    }
 
-            @Override
-            public void onConnected(SpotifyAppRemote remote) {
-                spotifyAppRemote = remote;
-                Log.d(TAG, "Connected to spotify app remote");
+    @Override
+    public void getNextPage(Pager<SavedTrack> currentPage) {
+        if (currentPage == null) {
+            getSpotifyTracks(0);
+            return;
+        }
 
-                SpotifyApi api = new SpotifyApi();
-                api.setAccessToken(getIntent().getStringExtra(HomeActivity.SPOTIFY_TOKEN_EXTRA));
-                spotifyWebApi = api.getService();
-                getSpotifyTracks(0);
-            }
-
-            @Override
-            public void onFailure(Throwable throwable) {
-                Log.e(TAG, throwable.getMessage(), throwable);
-            }
-        });
+        if (currentPage.next == null) return;
+        int nextOffset = currentPage.offset + currentPage.items.size();
+        getSpotifyTracks(nextOffset);
     }
 
     private void getSpotifyTracks(int offset) {
@@ -79,7 +48,8 @@ public class LikedSongsActivity extends AppCompatActivity implements PagerAdapte
         spotifyWebApi.getMySavedTracks(queryParams, new SpotifyCallback<Pager<SavedTrack>>() {
             @Override
             public void failure(SpotifyError spotifyError) {
-                Log.e(TAG, "failure to get saved tracks: " + spotifyError.getErrorDetails().message, spotifyError);
+                Log.e(TAG, "failure to get saved tracks: " +
+                        spotifyError.getErrorDetails().message, spotifyError);
             }
 
             @Override
@@ -87,36 +57,6 @@ public class LikedSongsActivity extends AppCompatActivity implements PagerAdapte
                 adapter.addPage(savedTrackPager);
             }
         });
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent keyEvent) {
-        Log.d(TAG, "onKeyDown: " + keyCode, null);
-
-        if (keyCode == KeyEvent.KEYCODE_DPAD_UP || keyCode == KeyEvent.KEYCODE_DPAD_DOWN) {
-            adapter.updateSelection(keyCode);
-            return true;
-        }
-
-        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER || keyCode == KeyEvent.KEYCODE_ENTER) {
-            SavedTrack savedTrack = adapter.getSelectedItem();
-            if (savedTrack != null && spotifyAppRemote != null) {
-                Log.d(TAG, "playing " + savedTrack.track.name + " with URI: " +
-                        savedTrack.track.uri);
-                spotifyAppRemote.getPlayerApi().play(savedTrack.track.uri);
-            }
-            return true;
-        }
-
-        return super.onKeyDown(keyCode, keyEvent);
-    }
-
-    @Override
-    public void getNextPage(Pager<SavedTrack> currentPage) {
-        if (currentPage.next == null) return;
-
-        int nextOffset = currentPage.offset + currentPage.items.size();
-        getSpotifyTracks(nextOffset);
     }
 
     @Override
---
+++ app/src/main/java/com/jackstenglein/sonimspotifyclient/list/AbstractListActivity.java
@@ -0,0 +1,80 @@
+package com.jackstenglein.sonimspotifyclient.list;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.jackstenglein.sonimspotifyclient.HomeActivity;
+import com.jackstenglein.sonimspotifyclient.R;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+
+public abstract class AbstractListActivity<T> extends AppCompatActivity implements PagerAdapter.DataSource<T> {
+
+    protected abstract String getTag();
+    protected abstract void handleSelection(T item);
+
+    protected PagerAdapter<T> adapter;
+    protected SpotifyService spotifyWebApi;
+    protected SpotifyAppRemote spotifyAppRemote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.primary_secondary_list);
+
+        RecyclerView songsList = findViewById(R.id.primarySecondaryList);
+        songsList.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        adapter = PagerAdapter.create(this, layoutManager);
+        songsList.setAdapter(adapter);
+        songsList.setLayoutManager(layoutManager);
+
+        connectToSpotifyAppRemote();
+    }
+
+    private void connectToSpotifyAppRemote() {
+        ConnectionParams connectionParams = new ConnectionParams.Builder(HomeActivity.CLIENT_ID)
+                .setRedirectUri(HomeActivity.REDIRECT_URI).showAuthView(false).build();
+        SpotifyAppRemote.connect(this, connectionParams, new Connector.ConnectionListener(){
+
+            @Override
+            public void onConnected(SpotifyAppRemote remote) {
+                spotifyAppRemote = remote;
+                Log.d(getTag(), "Connected to spotify app remote");
+
+                SpotifyApi api = new SpotifyApi();
+                api.setAccessToken(getIntent().getStringExtra(HomeActivity.SPOTIFY_TOKEN_EXTRA));
+                spotifyWebApi = api.getService();
+                getNextPage(null);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                Log.e(getTag(), throwable.getMessage(), throwable);
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent keyEvent) {
+        Log.d(getTag(), "onKeyDown: " + keyCode, null);
+
+        if (keyCode == KeyEvent.KEYCODE_DPAD_UP || keyCode == KeyEvent.KEYCODE_DPAD_DOWN) {
+            adapter.updateSelection(keyCode);
+            return true;
+        }
+
+        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER || keyCode == KeyEvent.KEYCODE_ENTER) {
+            handleSelection(adapter.getSelectedItem());
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, keyEvent);
+    }
+}
